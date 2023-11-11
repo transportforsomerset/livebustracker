0ca901dfb6c17b8e5b8e6c8daa921073b6612451
Transport for Somerset <141499229+transportforsomerset@users.noreply.github.com>
2023-11-11 23:08:47 +0000

    Create tracker.js

---
+++ docs/tracker.js
@@ -0,0 +1,16 @@
+async function trackingMap(ref) {
+  let trackingFile = "https://livebustracker.transportforsomerset.co.uk/tracking-data.geojson";
+  
+  const trackingFileAllData = await fetch(trackingFile);
+  const trackingData = await trackingFileAllData.json();
+
+  console.log("Ref: " + ref);
+}
+
+function startTracking() {
+  if (ref)
+    ref++;
+  else
+    let ref = 0;
+  setInterval(trackingMap(ref), 60000); // 1 minute.
+}
