e1158e0ec68ce5b8e0d07df031b8746ae899ebd4
Transport for Somerset <141499229+transportforsomerset@users.noreply.github.com>
2024-03-31 23:23:14 +0100

    Update tracker.js

--- docs/tracker.js
+++ docs/tracker.js
@@ -1,8 +1,14 @@
 async function trackingMap(counter) {
+  console.log("trackingMap() called ...");
+  
   let trackingFile = "https://livebustracker.transportforsomerset.co.uk/tracking-data.geojson";
+  console.log("trackingFile assigned ...");
   
   const trackingFileAllData = await fetch(trackingFile);
+  console.log("trackingFile called ...");
+  
   const trackingData = await trackingFileAllData.json();
+    console.log("trackingFile recieved ...");
 
   console.log("Counter: " + counter);
   return counter;
